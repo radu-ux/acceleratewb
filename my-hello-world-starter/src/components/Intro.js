6ce5f0d1b8e45766a2eae2209e6b33952cefe559
radu-ux <radu.uivari99@gmail.com>
2021-08-09 11:29:36 +0300

    New design update

--- my-hello-world-starter/src/components/Intro.js
+++ my-hello-world-starter/src/components/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import DiagramImg from '../images/image16.png';
+import DiagramImg from '../images/image16-min.png';
+import styled from 'styled-components';
 
 const HeroSectionBgImageStyle = {
 
@@ -7,9 +8,15 @@ const HeroSectionBgImageStyle = {
     backgroundSize:"cover",
 }
 
+const HeroSection = styled.section`
+    background: url(${DiagramImg}) no-repeat center center fixed;
+    background-size: cover    
+    
+`;
+
 const Intro = () => {
     return (
-        <section id="hero-section" className="flex flex-row" style={HeroSectionBgImageStyle}>
+        <HeroSection className="flex flex-row">
             <div className="flex flex-col mx-auto xl:py-24 2xl:py-52">
                 <h1 className="text-8xl font-bold text-center">Dezvoltam Solutii Web <span className="block text-center"> Smart & Eficiente</span></h1>
                 <div className="max-w-3xl my-5 mx-auto">
@@ -28,21 +35,8 @@ const Intro = () => {
                     </div>
                 </div>
             </div>
-        </section>
+        </HeroSection>
     )
 }
 
-const HeroSection = () =>  {
-    return (
-        <>
-        <div className="flex flex-col items-start">
-            <h1 className="text-4xl">Dezvoltam Solutii Web <br/> Smart si Eficiente</h1>
-            <div className="flex flex-row">
-                <button>Contacteaza-ne</button>
-                <p>0765.623.233</p>
-            </div>
-        </div>
-        </>
-    );
-}
 export default Intro;
