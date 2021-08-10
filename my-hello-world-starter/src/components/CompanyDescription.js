1024c95524dc41d44cc0cac33e88ef5029b63138
radu-ux <radu.uivari99@gmail.com>
2021-08-10 13:06:25 +0300

    Company description section added

--- my-hello-world-starter/src/components/CompanyDescription.js
+++ my-hello-world-starter/src/components/CompanyDescription.js
@@ -1,32 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import H2 from '../components/common/SharedComponents';
-import CompanyDescriptionImg from '../images/hero-img.jpg';
+import TeamWorkImg500 from '../images/team-work-500.jpg';
+import TeamWorkImg1000 from '../images/team-work-1000.jpg';
 import styled from 'styled-components';
+import AOS from 'aos'
 
-const Wrapper = styled.div`
-    img { 
-        width: 80%;
+const Wrapper = styled.div`   
+    img {
+        border-radius: 50%;
+        @media (min-width: 1024px) {
+            width: 480px;
+            height: 400px;
+        }
+
+        @media (min-width: 1536px) { 
+            width: 650px;
+            height: 500px;
+        }
     }
 `;
 
 const CompanyDescription = () => {
+    useEffect(() => {
+        AOS.init({duration: 2000});
+    })
+
     return (
         <section className="flex flex-col mt-20">
             <H2>Cine suntem ?</H2>
-            <div className="flex flex-row justify-center my-10">
-                <div className="flex flex-col self-center max-w-xl">
-                    <p className="text-2xl mx-5">
+            <div className="flex flex-col md:flex-row justify-center my-10">
+                <div data-aos="flip-left" className="flex flex-col self-center">
+                    <p className="sm:text-2xl 2xl:text-3xl mx-5 sm:max-w-xl 2xl:max-w-2xl">
                         Suntem o companie ce ne ocupam cu dezvoltarea de solutii web pentru afacerea ta.
                         Dispunem de o echipa flexibila, capabila de a-ti ajuta compania sa-si creasca 
                         veniturile prin intermediul onlineului.
                     </p>
-                    <button className="text-lg bg-blue-500 py-2 w-40 mt-5 mx-auto rounded-full text-white shadow-lg hover:bg-blue-700">Cere oferta</button>
+                    <button className="text-lg bg-yellow-500 py-2 w-40 mt-5 mx-auto rounded-full text-white border-2 ring ring-yellow-500 shadow-lg hover:bg-yellow-400">Cere oferta</button>
                 </div>
-                <Wrapper>
-                   <img className="rounded-full"
+                <Wrapper className="self-center mt-8 md:mt-0">
+                   <img width="500"
+                        height="500"
                         src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/308367/cat-500.jpg"
-                        srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/308367/cat-500.jpg 500w,
-                               https://s3-us-west-2.amazonaws.com/s.cdpn.io/308367/cat-1000.jpg 1000w"
+                        srcset={
+                                `${TeamWorkImg500} 500w,
+                                 ${TeamWorkImg1000} 1000w`
+                               }
                         sizes="(min-width: 600px) 50vw, 100vw" >        
                 </img>
                 </Wrapper>
