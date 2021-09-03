a488ccb6efec87dd7d8def86f6b9719ca45e38ae
radu-ux <radu.uivari99@gmail.com>
2021-09-03 20:32:50 +0300

    Modified Hero Section to for mobile devices

--- my-hello-world-starter/src/components/CompanyDescription.js
+++ my-hello-world-starter/src/components/CompanyDescription.js
@@ -19,11 +19,13 @@ const PhoneIconWrapper = styled.p`
 
 const StyledSection = styled.section`
         height: 100vh;
+        height: calc(var(--vh, 1vh) * 100);
         background: url(${WavesMobile}) center ;
         background-size: cover;
         background-repeat: no-repeat;
 
         @media(max-width: 480px) {
+            height: 100%;
             background: url(${WavesMobile}) center ;
             background-size: cover;
             background-repeat: no-repeat;
@@ -64,18 +66,18 @@ const CompanyDescription = () => {
     }
 
     return (
-         <StyledSection className="flex flex-col lg:flex-row items-center py-16 space-x-8 space-y-10 px-8 sm:px-14 2xl:px-32 lg:space-y-0">
-               <div className="flex flex-col max-w-4xl mt-5 space-y-5 mx-5 self-center  md:mx-0 lg:space-y-10 xl:mt-10 2xl:mb-28">
+         <StyledSection className="flex flex-col lg:flex-row items-center py-16 space-x-8 space-y-10 px-3 sm:px-14 2xl:px-32 lg:space-y-0">
+               <div className="flex flex-col sm:max-w-4xl mt-5 space-y-5 mx-5 self-center md:mx-0 lg:space-y-10 xl:mt-10 2xl:mb-28">
                    <h1 className="font-bold text-white md:text-4xl text-3xl lg:text-5xl xl:text-5xl 2xl:text-6xl">Inovam afacerea ta prin websiteuri moderne si ultra rapide</h1>
                    <div className="text-white xl:max-w-lg 2xl:max-w-2xl md:text-lg xl:text-xl 2xl:text-2xl space-y-5">
                         <p>Suntem o companie ce oferim solutii vaste pentru nevoile tale, de la siteuri de prezentare, la magazine online si chiar apliatii mobile</p>
                         <p> <span className="text-3xl 2xl:text-4xl font-bold">!</span> Nu ezita sa ne contactezi pentru orice intrebare</p>
                     </div>
-                   <div className="flex flex-row space-x-5 self-center ">
+                   <div className="flex flex-row self-center">
                         <ContactButton/>
                     </div>
                </div>
-               <div className="w-10/12 sm:w-8/12 lg:w-full xl:w-11/12 shadow-xl">
+               <div className="mx-auto w-10/12 sm:w-8/12 lg:w-full xl:w-11/12 shadow-xl">
                     <Img fluid={heroImageProperties.file.childImageSharp.fluid} /> 
                 </div>
             </StyledSection>
